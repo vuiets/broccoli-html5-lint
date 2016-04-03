8be7354b34fbedcb52a48a97ad94414e5c93281e
Vishak <wizvishak@gmail.com>
2016-04-03 16:46:37 +0530

    Make broccoli-html5-lint
    Lint HTML files
    
    Format outputs

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
-# broccoli-editorconfig
+# broccoli-html5-lint
 
-Code formatter for [Broccoli](https://github.com/joliss/broccoli) with [.editorconfig](https://github.com/editorconfig/editorconfig-core-js).
+Run [HTML5 lint](https://github.com/mozilla/html5-lint) on [broccoli](https://github.com/joliss/broccoli) nodes.
 
 ## Installation
 
 Install with [npm](https://github.com/npm/npm#npm1----node-package-manager). (Make sure you have installed [Node](http://nodejs.org/).)
 
 ```
-npm install --save-dev broccoli-editorconfig
+npm install --save-dev broccoli-html5-lint
 ```
 
 ## Usage
 
 ```javascript
-var codeStyle = require('broccoli-editorconfig');
-tree = codeStyle(tree, options);
+var HTML5Lint = require('broccoli-html5-lint');
+node = new HTML5Lint(inputNodes, options);
 ```
+
+## License
+MIT
--- index.js
+++ index.js
@@ -1,18 +1,112 @@
 'use strict';
 
-import Filter from 'broccoli-filter';
-import editorconfig from 'editorconfig';
+const Filter = require('broccoli-persistent-filter'),
+	html5Lint = require('html5-lint'),
+	Promise = require('rsvp').Promise;
 
-EditorConfigStyle.prototype = Object.create(Filter.prototype);
-EditorConfigStyle.prototype.constructor = EditorConfigStyle;
+function pluralize(word, count) {
+	return (count === 1 ? word : `${word}s`);
+}
 
-EditorConfigStyle = (inputNode, options) => {
-	if(!(this instanceof EditorConfigStyle)) {
-		return new EditorConfigStyle(inputNode, options);
-	}
+function formatOp(filePath, messages) {
+	// lazy load output format helper libs
+	const chalk = require('chalk'),
+		table = require('text-table');
 
-	this.options = options || {};
-	Filter.call(this, inputNode);
+	let output = '\n',
+		infos = 0,
+		warnings = 0,
+		errors = 0,
+		total = 0,
+		summaryColor = 'yellow';
+
+	output += chalk.underline(`${filePath}\n\n`);
+
+	const msgRows = messages.map((message) => {
+		const lineNumber = [message.lastLine, message.firstColumn].join(':');
+		let messageType = message.type;
+
+		switch (messageType) {
+			case 'info':
+				messageType = chalk.cyan(messageType);
+				infos++;
+				break;
+			case 'error':
+				messageType = chalk.red(messageType);
+				errors++;
+				summaryColor = 'red';
+				break;
+			case 'default':
+				messageType = chalk.white(messageType)
+		}
+
+		return [
+			'',
+			chalk.dim(lineNumber),
+			messageType,
+			message.message,
+			''
+		]
+	});
+
+	total = errors + warnings + infos;
+
+	output += table(msgRows, {
+			align: ['', 'c', 'c', 'l', ''],
+			stringLength: function (str) {
+				return chalk.stripColor(str).length;
+			}
+		}) + '\n\n';
+
+	output += chalk[summaryColor].bold([
+		'\u2716 ',
+		total, pluralize(' problem', total),
+		' (',
+		errors, pluralize(' error', errors),
+		', ',
+		warnings, pluralize(' warning', warnings),
+		', ',
+		infos, pluralize(' info', infos),
+		')\n'
+	].join(''));
+
+	output += '\n\n';
+
+	console.log(output);
+}
+
+function HTML5Lint(inputNode, options) {
+	if (!(this instanceof HTML5Lint)) return new HTML5Lint(inputNode, options);
+
+	Filter.call(this, inputNode, options);
+
+	this.options = options;
+}
+
+HTML5Lint.prototype = Object.create(Filter.prototype);
+HTML5Lint.prototype.constructor = HTML5Lint;
+
+HTML5Lint.prototype.processString = function (content, relativePath) {
+	const promise = new Promise((resolve, reject) => {
+		html5Lint(content, (err, results) => {
+			if (err) reject(err);
+
+			if (results.messages) {
+				formatOp(relativePath, results.messages);
+			}
+
+			resolve(content);
+		});
+
+	});
+
+	return promise.then((content) => {
+		return content;
+	}).catch((error) => {
+		throw error;
+	});
 };
 
-export default EditorConfigStyle;
+HTML5Lint.prototype.extensions = ['html'];
+
+module.exports = HTML5Lint;
